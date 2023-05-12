cfd65b6a4ffc7b827449630d0885e2200092e632
rodion1682 <rodiontropkin17@gmail.com>
2023-05-12 20:45:14 +0300

    started building

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div className="App">
+            <div>app</div>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/app/api/user.api.js
@@ -0,0 +1,42 @@
+const users = [
+    {
+        id: 0,
+        name: "Name",
+        surname: "Surname",
+        age: 20,
+        image: "https://www.w3schools.com/howto/img_avatar.png",
+        about: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti delectus magnam beatae, distinctio mollitia cum sunt illum quae neque veritatis voluptatibus officiis dignissimos ipsum nobis ullam natus est ratione exercitationem?"
+    },
+    {
+        id: 1,
+        name: "Name 1",
+        surname: "1 Surname",
+        age: 20,
+        image: "https://www.w3schools.com/howto/img_avatar.png",
+        about: "1 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti delectus magnam beatae, distinctio mollitia cum sunt illum quae neque veritatis voluptatibus officiis dignissimos ipsum nobis ullam natus est ratione exercitationem?"
+    },
+    {
+        id: 2,
+        name: "Name 2",
+        surname: "2 Surname",
+        age: 20,
+        image: "https://www.w3schools.com/howto/img_avatar.png",
+        about: "2 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti delectus magnam beatae, distinctio mollitia cum sunt illum quae neque veritatis voluptatibus officiis dignissimos ipsum nobis ullam natus est ratione exercitationem?"
+    },
+    {
+        id: 3,
+        name: "Name 3",
+        surname: "3 Surname",
+        age: 20,
+        image: "https://www.w3schools.com/howto/img_avatar.png",
+        about: "3 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti delectus magnam beatae, distinctio mollitia cum sunt illum quae neque veritatis voluptatibus officiis dignissimos ipsum nobis ullam natus est ratione exercitationem?"
+    },
+    {
+        id: 4,
+        name: "Name 4",
+        surname: "4 Surname",
+        age: 20,
+        image: "https://www.w3schools.com/howto/img_avatar.png",
+        about: "4 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti delectus magnam beatae, distinctio mollitia cum sunt illum quae neque veritatis voluptatibus officiis dignissimos ipsum nobis ullam natus est ratione exercitationem?"
+    }
+];
