b7b03b6ee7052a7a584198c03bfe47b6035a53e1
NakhimoVV <tj1993@bk.ru>
2023-05-13 14:14:34 +0300

    created Loading common

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Loading from "./app/components/common/loading";
+import "./style.css";
+
 function App() {
     return (
         <div className="App">
             <div>app</div>
+            <Loading />
         </div>
     );
 }
