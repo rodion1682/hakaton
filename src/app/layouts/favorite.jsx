0e343e6fb0729b58be2b1306136f44d84ab68bf3
rodion1682 <rodiontropkin17@gmail.com>
2023-05-13 15:07:26 +0300

    merged files

--- src/app/layouts/favorite.jsx
+++ src/app/layouts/favorite.jsx
@@ -1,5 +1,33 @@
+import React, { useState } from "react";
+import user from "../api/user.api";
+
 const Favorite = () => {
-	return <div>Favorite</div>;
+    const firstUserId = 1;
+
+    const [users, setUsers] = useState([]);
+    user.getById(firstUserId).then((user) => {
+        setUsers(user);
+    });
+    console.log(users);
+    return (
+        <div>
+            <div className="d-flex align-items-start pb-5">
+                <div className="d-flex flex-grow-1 me-5">
+                    <div className="me-4">
+                        <img
+                            className="img-fluid"
+                            src={users.image}
+                            alt="image"
+                        />
+                    </div>
+                    <div>
+                        {users.name} {users.surname}
+                    </div>
+                </div>
+                <button>button delete</button>
+            </div>
+        </div>
+    );
 };
 
 export default Favorite;
