fd05e8e383046e49f9322a497a3a34d017931e99
rodion1682 <rodiontropkin17@gmail.com>
2023-05-13 09:39:21 +0300

    create react-router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import NavBar from "./app/components/ui/navBar";
+import { Redirect, Route, Switch } from "react-router-dom";
+import Favorite from "./app/layouts/favorite";
+import Main from "./app/layouts/main";
+import MemberPage from "./app/layouts/memberPage";
+
 function App() {
     return (
         <div className="App">
+            <NavBar />
             <div>app</div>
+            <Switch>
+                <Route path="/" exact component={Main} />
+                <Route path="/favorite" component={Favorite} />
+                <Route path="/memberPage" component={MemberPage} />
+                <Route path="/memberPage/:memberId" component={MemberPage} />
+                <Redirect to="/" />
+            </Switch>
         </div>
     );
 }
---
+++ src/app/layouts/memberPage.jsx
@@ -0,0 +1,5 @@
+const MemberPage = () => {
+    return <div></div>;
+};
+
+export default MemberPage;
