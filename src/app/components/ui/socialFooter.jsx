5f70dd3c4625995cd5d0537268fedfc5040aba89
Igor Zaytsev <zaytsevie@gmail.com>
2023-05-14 11:25:09 +0400

    socialFooter_fix

--- src/app/components/ui/socialFooter.jsx
+++ src/app/components/ui/socialFooter.jsx
@@ -18,26 +18,6 @@ const SocialFooter = ({ social }) => {
                     </li>
                 ))}
         </ul>
-
-        // <>
-        //     <ul className="nav justify-content-center fs-6">
-        //         <li className="nav-item">
-        //             <a className="btn" target="_blank" href="#">
-        //                 Instagram <i className="bi bi-instagram"></i>
-        //             </a>
-        //         </li>
-        //         <li className="nav-item">
-        //             <a className="btn" target="_blank" href="#">
-        //                 Telegram <i className="bi bi-telegram"></i>
-        //             </a>
-        //         </li>
-        //         <li className="nav-item">
-        //             <a className="btn" target="_blank" href="#">
-        //                 В контакте <i className="bi bi-bootstrap"></i>
-        //             </a>
-        //         </li>
-        //     </ul>
-        // </>
     );
 };
 
